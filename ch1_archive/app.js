7066dff500d21a474ce76543a722e2b0e84fc0cb
Chris Mason <chrismason285@gmail.com>
2017-07-17 19:51:37 -0700

    lessons 2.1-2.5: images and filters

--- ch1_archive/app.js
+++ ch1_archive/app.js
@@ -9,33 +9,17 @@
                 name: 'Dodecahedron',
                 price: 2.00,
                 description: 'Gems are actually of little to no real value.  Something like horse manure will prove far more desirable in the post-apocalyptic wastes as fertilizer for delicious rad-shrooms',
-                images: [ 
-                {
-                        full: 'resources/dodecahedron-01-full.jpg',
-                        thumb: 'resources/dodecahedron-01-thumb.jpg',
-                },
-                {
-                        full: 'resources/dodecahedron-02-full.jpg',
-                        thumb: 'resources/dodecahedron-02-thumb.jpg',
-                }
-                ]
-
+                canPurchase: true,
+                soldOut: false,
         },
         {
                 name: "Penatagonal Gem",
                 price: 5.95,
                 description: "This is another gem that of course has no real value, but this one shares a shape name with a satanic symbol, which at least makes it more likely that the irradiated ghouls of the wastes will fear you for possessing it.",
-                images: [ 
-                {
-                        full: 'resources/pentagonal-01-full.jpg',
-                        thumb: 'resources/pentagonal-01-thumb.jpg',
-                },
-                {
-                        full: 'resources/pentagonal-02-full.jpg',
-                        thumb: 'resources/pentagonal-02-thumb.jpg',
-                }
-                ]
+                canPurchase: true,
+                soldOut: false,
         },
+
         ];
 
 })();
