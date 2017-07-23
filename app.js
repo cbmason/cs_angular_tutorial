f3f292989b7b11459d00e508421e782bab3ced24
Chris Mason <chrismason285@gmail.com>
2017-07-23 14:49:44 -0700

    Lessons 3.1-3.7: forms / previews / input

--- app.js
+++ app.js
@@ -5,18 +5,37 @@
                 this.products = gems;
         });
 
-        app.controller('PanelController', function(){
+        app.controller('TabController', function(){
                 this.tab = 1;
 
-                this.selectTab = function(setTab) {
-                        this.tab = setTab;
+                this.setTab = function(tab) {
+                        this.tab = tab;
                 };
 
-                this.isSelected = function(checkTab){
-                        return this.tab === checkTab;
+                this.isSet = function(tab){
+                        return this.tab === tab;
                 };
 
         });
+
+        app.controller("GalleryController", function(){
+                this.current = 0;
+
+                this.setCurrent = function(index){
+                        this.current = index;
+                };
+        });
+
+
+        app.controller("ReviewController", function(){
+                this.review = {};
+                this.addReview = function(product) {
+                        product.reviews.push(this.review);
+                        this.review = {};
+                };
+
+        });
+
 
         var gems = [
         {
@@ -24,14 +43,21 @@
                 price: 2.00,
                 description: 'Gems are actually of little to no real value.  Something like horse manure will prove far more desirable in the post-apocalyptic wastes as fertilizer for delicious rad-shrooms',
                 images: [ 
+                        'resources/dodecahedron-01-full.jpg',
+                        'resources/dodecahedron-01-thumb.jpg',
+                        'resources/dodecahedron-02-thumb.jpg',
+                ],
+                reviews: [
                 {
-                        full: 'resources/dodecahedron-01-full.jpg',
-                        thumb: 'resources/dodecahedron-01-thumb.jpg',
+                        stars: 5,
+                        body: "I love useless rocks!",
+                        author: "joe@mama.com"
                 },
                 {
-                        full: 'resources/dodecahedron-02-full.jpg',
-                        thumb: 'resources/dodecahedron-02-thumb.jpg',
-                }
+                        stars: 1,
+                        body: "This product sucks",
+                        author: "w33dl0rd69@dbag.com"
+                },
                 ]
 
         },
@@ -40,15 +66,10 @@
                 price: 5.95,
                 description: "This is another gem that of course has no real value, but this one shares a shape name with a satanic symbol, which at least makes it more likely that the irradiated ghouls of the wastes will fear you for possessing it.",
                 images: [ 
-                {
-                        full: 'resources/pentagonal-01-full.jpg',
-                        thumb: 'resources/pentagonal-01-thumb.jpg',
-                },
-                {
-                        full: 'resources/pentagonal-02-full.jpg',
-                        thumb: 'resources/pentagonal-02-thumb.jpg',
-                }
-                ]
+                        'resources/pentagonal-01-full.jpg',
+                        'resources/pentagonal-01-thumb.jpg',
+                        'resources/pentagonal-02-thumb.jpg',
+                ],
         },
         ];
 
